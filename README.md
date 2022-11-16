27bd5b65f06f337d2091db03b4ad5946ec1fe06c
Yoann Dufresne <yoann.dufresne0@gmail.com>
2022-11-16 16:01:49 +0100

    solution TP1 complet

--- README.md
+++ README.md
@@ -1,3 +1,73 @@
 
-# Séquences
-* https://www.ncbi.nlm.nih.gov/data-hub/genome/GCF_000006945.2/
+# Résultats
+
+## Exec time / RAM
+
+k = 21
+
+### dictionnaires
+
+```
+/usr/bin/time -v python3 main.py 
+index
+GCF_000006945.2_ASM694v2_genomic.fna GCF_008244785.1_ASM824478v1_genomic.fna 0.9377423900044074 (0.9599864198488329, 0.9758858885062132)
+
+	Command being timed: "python3 main.py"
+	User time (seconds): 9.72
+	System time (seconds): 0.29
+	Percent of CPU this job got: 99%
+	Elapsed (wall clock) time (h:mm:ss or m:ss): 0:10.02
+	Average shared text size (kbytes): 0
+	Average unshared data size (kbytes): 0
+	Average stack size (kbytes): 0
+	Average total size (kbytes): 0
+	Maximum resident set size (kbytes): 934072
+	Average resident set size (kbytes): 0
+	Major (requiring I/O) page faults: 0
+	Minor (reclaiming a frame) page faults: 308576
+	Voluntary context switches: 1
+	Involuntary context switches: 84
+	Swaps: 0
+	File system inputs: 0
+	File system outputs: 0
+	Socket messages sent: 0
+	Socket messages received: 0
+	Signals delivered: 0
+	Page size (bytes): 4096
+	Exit status: 0
+
+
+```
+
+### Listes
+
+```
+/usr/bin/time -v python3 main.py 
+lists
+GCF_000006945.2_ASM694v2_genomic.fna GCF_008244785.1_ASM824478v1_genomic.fna 0.9377423900044074 (0.9599864198488329, 0.9758858885062132)
+
+	Command being timed: "python3 main.py"
+	User time (seconds): 9.04
+	System time (seconds): 0.16
+	Percent of CPU this job got: 100%
+	Elapsed (wall clock) time (h:mm:ss or m:ss): 0:09.20
+	Average shared text size (kbytes): 0
+	Average unshared data size (kbytes): 0
+	Average stack size (kbytes): 0
+	Average total size (kbytes): 0
+	Maximum resident set size (kbytes): 567176
+	Average resident set size (kbytes): 0
+	Major (requiring I/O) page faults: 0
+	Minor (reclaiming a frame) page faults: 163235
+	Voluntary context switches: 1
+	Involuntary context switches: 70
+	Swaps: 0
+	File system inputs: 0
+	File system outputs: 0
+	Socket messages sent: 0
+	Socket messages received: 0
+	Signals delivered: 0
+	Page size (bytes): 4096
+	Exit status: 0
+
+```
--- main.py
+++ main.py
@@ -14,10 +14,25 @@ def index_file(sequences, k):
     return index
 
 
+def list_file(sequences, k):
+    lst = []
+
+    # Construct the list of all the kmers of all the sequences
+    for seq in sequences:
+        lst.extend([min(x) for x in stream_kmers(seq, k)])
+
+    # Sort the list
+    lst.sort()
+
+    return lst
+
+
 def intersect_index(index, sequences, k):
+    """ Create an index containing all the kmers of the input sequences
+    """
     index_uniq = index.copy()
-    query_uniq = {}
-    intersection = {}
+    query_uniq = 0
+    intersection = 0
 
     for seq in sequences:
         for kmer, rkmer in stream_kmers(seq, k):
@@ -25,34 +40,57 @@ def intersect_index(index, sequences, k):
 
             # Query not in index
             if minmer not in index_uniq:
-                if minmer not in query_uniq:
-                    query_uniq[minmer] = 0
-                query_uniq[minmer] += 1
+                query_uniq += 1
             # Query in index => intersection
             else:
-                if minmer not in intersection:
-                    intersection[minmer] = 0
-                intersection[minmer] += 1
+                intersection += 1
                 index_uniq[minmer] -= 1
                 if index_uniq[minmer] == 0:
                     del index_uniq[minmer]
 
-    return index_uniq, intersection, query_uniq
+    return sum(index_uniq.values()), intersection, query_uniq
+
+
+def intersect_sorted_lists(lst1, lst2):
+    idx1 = idx2 = 0
+    # Dataset specific or intersection kmer counts
+    A = inter = B = 0
+
+    while (idx1 < len(lst1) and idx2 < len(lst2)):
+        kmer1 = lst1[idx1]
+        kmer2 = lst2[idx2]
+
+        # Same kmer => intersection
+        if kmer1 == kmer2:
+            inter += 1
+            idx1 += 1
+            idx2 += 1
+        # first list specific
+        elif kmer1 < kmer2:
+            A += 1
+            idx1 += 1
+        # second list specific
+        else:
+            B += 1
+            idx2 += 1
+
+    # Add remaining kmers of the non empty list
+    A += len(lst1) - idx1
+    B += len(lst2) - idx2
+
+    return A, inter, B
 
 
 def similarity(A, inter, B):
-    inter_total =  sum(inter.values())
-    A_similarity = inter_total / (inter_total + sum(A.values()) + 1)
-    B_similarity = inter_total / (inter_total + sum(B.values()) + 1)
+    # +1 added for pseudocount. Avoid divisions by 0
+    A_similarity = inter / (inter + A + 1)
+    B_similarity = inter / (inter + B + 1)
 
     return A_similarity, B_similarity
 
 
 def jaccard(A, inter, B):
-    inter_total =  sum(inter.values())
-    total = inter_total + sum(A.values()) + sum(B.values())
-
-    return inter_total / total
+    return inter / (A + inter + B)
 
 
 
@@ -60,12 +98,22 @@ if __name__ == "__main__":
     # Load all the files in a dictionary
     files = load_directory("data")
 
-    k = 13
+    k = 21
 
+    # Loading
     indexes = {f:index_file(files[f], k) for f in files}
+    lists = {f:list_file(files[f], k) for f in files}
     
     filenames = list(files.keys())
     for i in range(len(files)):
         for j in range(i+1, len(files)):
+            # Method 1 using index
+            print("index")
             A, inter, B = intersect_index(indexes[filenames[i]], files[filenames[j]], k)
             print(filenames[i], filenames[j], jaccard(A, inter, B), similarity(A, inter, B))
+
+            # Method 2 using sorted lists
+            print("lists")
+            A, inter, B = intersect_sorted_lists(lists[filenames[i]], lists[filenames[j]])
+            print(filenames[i], filenames[j], jaccard(A, inter, B), similarity(A, inter, B))
+            print()
